ce1631b5bf942abca9e1d568d2a3411386be5036
Bethany Ramirez <bethany.e.ramirez@gmail.com>
2019-03-14 15:55:59 -0500

    addmovies function working and pushing to console

--- src/api.js
+++ src/api.js
@@ -1,6 +1,22 @@
-module.exports = {
-  getMovies: () => {
-    return fetch('/api/movies')
+const url=('/api/movies');
+
+const getMovies = () => {
+  return fetch(url)
       .then(response => response.json());
-  }
 };
+
+const addMovie =  (title, rating, id) =>{
+    const newMovie = {title: title, rating: rating, id: id};
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newMovie),
+      };
+    fetch(url,options)
+        .then(()=> console.log('new movie added'))
+        .catch(()=> console.log('error'))
+    };
+
+export default {getMovies, addMovie};
--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
 
-getMovies().then((movies) => {
+import movies from './api.js';
+
+movies.getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
@@ -18,3 +19,6 @@ getMovies().then((movies) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+movies.addMovie('Matrix', '3', '3')
+
